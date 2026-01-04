32860bd98ac06c7b79e4c8d0a501fdf7dc903d59
felipe <felipe.alvessival0710@gmail.com>
2026-01-04 17:52:00 -0300

    aula 17

---
+++ aula js/Array/aula17.js
@@ -0,0 +1,25 @@
+const alunos = ['felipe','alex'];
+
+ alunos.push('kaka')
+ console.log(alunos[2]);
+
+
+//numero.shift();// remover primeiro item da lista 
+
+//numero.splice(1, 1); remove qualquer posicao
+
+//numero.unshift('0'); adiciona no inicio;
+
+//push  adicionar no final elemento
+
+//pop remove ultimo
+
+
+
+const numero = ['1','2','3'];
+
+console.log(numero.shift());
+console.log(numero.splice(0,1));
+console.log(numero.pop());
+console.log(numero);
+                         
