698af2e2df79b732f72e1b74fb12a5564765a9ce
felipe <felipe.alvessival0710@gmail.com>
2026-01-07 20:25:17 -0300

    aula18 e 19

---
+++ aula js/Array/aula18.js
@@ -0,0 +1,11 @@
+const notas =[];
+
+notas.push(5);
+notas.push(5);
+notas.push(5);
+
+
+const soma = notas[0]+ notas[1]+ notas[2];
+console.log (soma/3);
+
+console.log(notas.length);
---
+++ estrutura de repeticao/aula19.js
@@ -0,0 +1,17 @@
+const notas =[];
+
+notas.push(5);
+notas.push(5);
+notas.push(5);
+
+
+const soma = notas[0]+ notas[1]+ notas[2];
+console.log (soma/3);
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+console.log(notas.length);
+
