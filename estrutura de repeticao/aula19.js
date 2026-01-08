b44ec0aae386df584afb1b67b3072eeac150b225
alexalves011 <alexalves.work@gmail.com>
2026-01-07 23:59:51 -0300

    aula 20

--- estrutura de repeticao/aula19.js
+++ estrutura de repeticao/aula19.js
@@ -8,10 +8,13 @@ notas.push(5);
 const soma = notas[0]+ notas[1]+ notas[2];
 console.log (soma/3);
 
-for (let index = 0; index < array.length; index++) {
+for (let index = 0; index < array.length; index++) {   
     const element = array[index];
     
 }
 
-console.log(notas.length);
+console.log(notas.length);                
+
+//i++ incrementa variavel
+
 
---
+++ estrutura de repeticao/aula20.js
@@ -0,0 +1,7 @@
+const nome = 'Felipe Alves da Silva';
+
+for (let i = 0; i< nome.length;i++ ){
+    console.log(nome[i]);
+}
+
+
